af1e208219ebd54fa618dcacbc1475151aff770a
Carlos Luna 201602491 <>
2017-08-16 14:16:22 -0600

    Creacion de menus de matrices

--- src/p1_201602491/MenuP_201602491.java
+++ src/p1_201602491/MenuP_201602491.java
@@ -1,16 +1,27 @@
 
 package p1_201602491;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
-  * @author Carlos Luna
+ * @author Carlos Luna
  */
 public class MenuP_201602491 {
+    
+    //Menu principial (Constructor)
     
     String start ;
     public int opcion;
     public boolean flag = true;
+    
+    public boolean flagm = true;
+    
+    Random rM = new Random();
+    
+    int[][] mPrincipiante = new int[4][4];
+    int[][] mIntermedio = new int[6][6];
+    int[][] mExperto = new int[8][8];
     
     public MenuP_201602491(){
         
@@ -22,6 +33,7 @@ public class MenuP_201602491 {
         this.start();
         this.menudificultad();
         this.selector();
+               
     }
     
     public void start(){
@@ -51,15 +63,15 @@ public class MenuP_201602491 {
             switch(opcion){
 
                 case 1:
-                   System.out.println("NIVEL PRINCIPIANTE");
+                    this.mPrincipiante();
                 break;
 
                 case 2:
-
+                    this.mIntermedio();
                 break;
 
                 case 3:
-
+                    this.mExperto();
                 break;
 
                 case 4:
@@ -67,6 +79,79 @@ public class MenuP_201602491 {
                 break;
             }
         }
+    }
+    
+    public void rmP(){
+        for(int i=0; i<=3; i++){
+            for(int j=0; j<=3; i++){
+                mPrincipiante[i][j] = rM.nextInt(15);
+            }
+        }
+    }
+    
+    public void rmI(){
+        for(int i=0; i<=5; i++){
+            for(int j=0; j<=5; i++){
+                mIntermedio[i][j] = rM.nextInt(15);
+            }
+        }
+    }
+    
+    public void rmE(){
+        for(int i=0; i<=7; i++){
+            for(int j=0; j<=7; i++){
+                mExperto[i][j] = rM.nextInt(15);
+            }
+        }
+    }
+    
+    public void mPrincipiante(){
+        
+        System.out.println("NIVEL PRINCIPIANTE");
+        for(int i=0; i<=3; i++){
+            System.out.println();
+            for(int j=0; j<=3; j++){
+               System.out.printf(" %s ",mPrincipiante[i][j]);
+            }
+        }
+        System.out.println("");
+        this.menuM();
+    }
+    
+    public void mIntermedio(){
+        
+        System.out.println("NIVEL INTERMEDIO");
+        for(int i=0; i<=5; i++){
+            System.out.println();
+            for(int j=0; j<=5; j++){
+                System.out.printf(" %s ",mIntermedio[i][j]);
+            }
+        }
+        System.out.println("");
+        this.menuM();
+    }
+    
+     public void mExperto(){
+        
+        System.out.println("NIVEL EXPERTO");
+        for(int i=0; i<=7; i++){
+            System.out.println();
+            for(int j=0; j<=7; j++){
+                System.out.printf(" %s ",mExperto[i][j]);
+            }
+        }
+        System.out.println("");
+        this.menuM();
+    } 
+     
+    public void menuM(){
+        
+        System.out.println("--------------------------");
+        System.out.println("1. Voltear");
+        System.out.println("2. Reiniciar");
+        System.out.println("3. Salir");
+        System.out.println("");
+        System.out.println("Ingresar opcion:");
     }
     
 }
